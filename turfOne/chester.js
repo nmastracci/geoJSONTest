f4ff12a34ad0438582e642ffdedfa45c30cb6496
nmastracci <nmastracci@carnegietechnologies.com>
2018-06-05 10:07:36 -0400

    adding turfjs and mapbox for cli

---
+++ turfOne/chester.js
@@ -0,0 +1,146 @@
+const chester = {
+    "type": "FeatureCollection",
+    "query": ["chester"],
+    "features": [{
+        "id": "place.8579228937725110",
+        "type": "Feature",
+        "place_type": ["place"],
+        "relevance": 1,
+        "properties": {
+            "wikidata": "Q959443"
+        },
+        "text": "Chesterfield",
+        "place_name": "Chesterfield, Missouri, United States",
+        "bbox": [-90.7324429985132, 38.5892000764067, -90.4808199800659, 38.6953830164871],
+        "center": [-90.5742, 38.6561],
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-90.5742, 38.6561]
+        },
+        "context": [{
+            "id": "region.28004",
+            "short_code": "US-MO",
+            "wikidata": "Q1581",
+            "text": "Missouri"
+        }, {
+            "id": "country.3145",
+            "short_code": "us",
+            "wikidata": "Q30",
+            "text": "United States"
+        }]
+    }, {
+        "id": "place.7751274879375880",
+        "type": "Feature",
+        "place_type": ["place"],
+        "relevance": 1,
+        "properties": {
+            "wikidata": "Q368045"
+        },
+        "text": "Chester",
+        "place_name": "Chester, Pennsylvania, United States",
+        "bbox": [-75.4239430023225, 39.822346029774, -75.343732254287, 39.8758250145105],
+        "center": [-75.3557, 39.8496],
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-75.3557, 39.8496]
+        },
+        "context": [{
+            "id": "region.3337",
+            "short_code": "US-PA",
+            "wikidata": "Q1400",
+            "text": "Pennsylvania"
+        }, {
+            "id": "country.3145",
+            "short_code": "us",
+            "wikidata": "Q30",
+            "text": "United States"
+        }]
+    }, {
+        "id": "place.16620007604375880",
+        "type": "Feature",
+        "place_type": ["place"],
+        "relevance": 1,
+        "properties": {
+            "wikidata": "Q1375884"
+        },
+        "text": "Chester",
+        "place_name": "Chester, Virginia, United States",
+        "bbox": [-77.5138990182064, 37.237342989833, -77.2441670284135, 37.4057500017771],
+        "center": [-77.4422, 37.3569],
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-77.4422, 37.3569]
+        },
+        "context": [{
+            "id": "region.228368",
+            "short_code": "US-VA",
+            "wikidata": "Q1370",
+            "text": "Virginia"
+        }, {
+            "id": "country.3145",
+            "short_code": "us",
+            "wikidata": "Q30",
+            "text": "United States"
+        }]
+    }, {
+        "id": "place.10125269452725110",
+        "type": "Feature",
+        "place_type": ["place"],
+        "relevance": 1,
+        "properties": {
+            "wikidata": "Q340608"
+        },
+        "text": "Chesterfield",
+        "place_name": "Chesterfield, Virginia, United States",
+        "bbox": [-77.8134947830995, 37.2498909814892, -77.4566909970257, 37.4678409954561],
+        "center": [-77.5785, 37.386],
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-77.5785, 37.386]
+        },
+        "context": [{
+            "id": "region.228368",
+            "short_code": "US-VA",
+            "wikidata": "Q1370",
+            "text": "Virginia"
+        }, {
+            "id": "country.3145",
+            "short_code": "us",
+            "wikidata": "Q30",
+            "text": "United States"
+        }]
+    }, {
+        "id": "neighborhood.2101994",
+        "type": "Feature",
+        "place_type": ["neighborhood"],
+        "relevance": 1,
+        "properties": {},
+        "text": "Chester Park/UMD",
+        "place_name": "Chester Park/UMD, Duluth, Minnesota 55812, United States",
+        "bbox": [-92.093474, 46.807133, -92.066291, 46.825869],
+        "center": [-92.09, 46.82],
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-92.09, 46.82]
+        },
+        "context": [{
+            "id": "postcode.13667359342723070",
+            "text": "55812"
+        }, {
+            "id": "place.5521230945447120",
+            "wikidata": "Q485708",
+            "text": "Duluth"
+        }, {
+            "id": "region.3382",
+            "short_code": "US-MN",
+            "wikidata": "Q1527",
+            "text": "Minnesota"
+        }, {
+            "id": "country.3145",
+            "short_code": "us",
+            "wikidata": "Q30",
+            "text": "United States"
+        }]
+    }],
+    "attribution": "NOTICE: © 2017 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service (https://www.mapbox.com/about/maps/). This response and the information it contains may not be retained."
+}
